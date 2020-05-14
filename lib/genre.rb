fe7bfba89d82deb068d6c7a17db9c53b00bf4d54
Jackson Ramos <jackramos021495@gmail.com>
2020-05-14 22:06:17 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,6 +11,11 @@ class Genre
 
     def new_song(name, artist)
       Song.new(name, artist, self)
+    end.all.select {|song| song.genre == self}
+
+    def artists
+      songs.collect {|song| song.artist}
+
     end
 
 end
